3333cc30f05c8d0c5f7b569d1f82dff45c21e465
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2025-05-25 15:12:29 +0200

    chore: add devenv to trusted substituters

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -27,4 +27,13 @@
     "flakes"
     "nix-command"
   ];
+
+  # Trusted substituters
+  nix.settings.trusted-substituters = [
+     "https://devenv.cachix.org"
+  ];
+
+  nix.settings.trusted-public-keys = [
+     "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+  ];
 }
