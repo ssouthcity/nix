2dbc23c72b1949da317e9d4d3907c7cd64790db7
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2026-04-13 15:58:34 +0200

    fix: use new lsp options for neovim

---
+++ modules/features/nixvim/lsp.nix
@@ -0,0 +1,47 @@
+{
+  flake.homeModules.nixvim = {
+    programs.nixvim.plugins.lsp.enable = true;
+
+    programs.nixvim.lsp = {
+      servers = {
+        gopls.enable = true;
+        hls.enable = true;
+        nixd.enable = true;
+        rust_analyzer.enable = true;
+        templ.enable = true;
+        terraformls.enable = true;
+        yamlls.enable = true;
+      };
+      keymaps = [
+        {
+          key = "K";
+          lspBufAction = "hover";
+        }
+        {
+          key = "gD";
+          lspBufAction = "references";
+        }
+        {
+          key = "gd";
+          lspBufAction = "definition";
+        }
+        {
+          key = "gi";
+          lspBufAction = "implementation";
+        }
+        {
+          key = "gt";
+          lspBufAction = "type_definition";
+        }
+        {
+          key = "<leader>rn";
+          lspBufAction = "rename";
+        }
+        {
+          key = "<leader>ca";
+          lspBufAction = "code_action";
+        }
+      ];
+    };
+  };
+}
