ef63d43aa0bede62f31fb2b71e36594d96852c54
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2026-04-23 21:37:23 +0200

    feat: add boot style and openrgb (#156)

--- modules/hosts/neptr/configuration.nix
+++ modules/hosts/neptr/configuration.nix
@@ -21,9 +21,30 @@
     {
       # Bootloader
       boot.loader = {
-        timeout = null; # wait indefinitely
+        timeout = 0;
         efi.canTouchEfiVariables = true;
         systemd-boot.enable = true;
+      };
+
+      # Boot animations
+      boot.plymouth = {
+        enable = true;
+        theme = "pixels";
+        themePackages = [
+          (pkgs.adi1090x-plymouth-themes.override {
+            selected_themes = [ "pixels" ];
+          })
+        ];
+      };
+
+      boot = {
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        kernelParams = [
+          "quiet"
+          "udev.log_level=3"
+          "systemd.show_status=auto"
+        ];
       };
 
       # Network
